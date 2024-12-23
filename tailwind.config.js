70d6c2166a31c3a28cf99521557d36214765ca65
Shahriar <shahriarkawsik@gmail.com>
2024-12-23 18:57:42 +0600

    add some utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color1: "rgb(16, 15, 15)", //title
+        color2: "rgb(64, 63, 63)", //card title
+        color3: "rgb(112, 111, 111)", //card subtitle and form placeholder
+        color4: "rgb(255, 193, 7)", //use it in button
+        color5: "rgba(255, 193, 7, 0.05)", //footer bg color
+      },
+      fontFamily: {
+        Rochester: "Rochester",
+        Poppins: "Poppins",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
