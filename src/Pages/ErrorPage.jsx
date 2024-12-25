d0da22645aa96945477ef5508d399224fa26dff2
Shahriar <shahriarkawsik@gmail.com>
2024-12-26 00:43:33 +0600

    add dynamic title for all route

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,7 +1,13 @@
+import { useLocation } from "react-router-dom";
 import error404 from "../assets/error404.json";
 import Lottie from "lottie-react";
+import { useEffect } from "react";
 
 const ErrorPage = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    document.title = "Error | FoodBites";
+  }, [pathname]);
   return (
     <div className="h-screen">
       <Lottie className="h-[700px]" animationData={error404}></Lottie>
